90f004b16eef53816465730104fba2cdfc5954a6
ahazain <ahmadfariddd5@gmail.com>
2024-11-14 13:12:48 +0700

    tes

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const PORT = 3000;
 
-// app.use("/", route);
-// route.get("/data", async function name(req, res) {
-//   const data = await prisma.nama.findMany();
-//   return res
-//     .status(200)
-//     .json({ status: 200, message: "berhasil", datanya: data });
-// });
+app.use("/", route);
+route.get("/data", async function name(req, res) {
+  const data = await prisma.nama.findMany();
+  return res
+    .status(200)
+    .json({ status: 200, message: "berhasil", datanya: data });
+});
 app.use("/", (req, res) => {
   res.send("halo rek === coba");
 });
