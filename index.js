b75ff497e416d29f6547cd708f51a14199f52ac6
ahazain <ahmadfariddd5@gmail.com>
2024-11-14 11:50:59 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const route = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const PORT = 3000;
+
+app.use("/", route);
+route.get("/data", async function name(req, res) {
+  const data = await prisma.nama.findMany();
+  return res
+    .status(200)
+    .json({ status: 200, message: "berhasil", datanya: data });
+});
+app.use("/", (req, res) => {
+  res.send("halo rek === coba");
+});
+
+app.listen(PORT, () => {
+  console.log(`I LOVE YOU ${PORT}`);
+});
---
+++ prisma/migrations/20241114034718_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "nama" (
+    "id" SERIAL NOT NULL,
+    "nama" TEXT NOT NULL,
+
+    CONSTRAINT "nama_pkey" PRIMARY KEY ("id")
+);
